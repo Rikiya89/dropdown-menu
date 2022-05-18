8ac99f44ab4231eb2875cae7eb6d8f5b4236c1ab
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2022-05-19 01:24:21 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let toggleButton = document.querySelector('.container');
+toggleButton.onclick = function() {
+    toggleButton.classList.toggle("active")
+}
+
+// let toggleButton = document.querySelector('.container');
+// toggleButton.onclick = function() {
+//     toggleButton.classList.toggle("active")
+// }
